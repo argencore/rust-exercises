9efdbefafbe748e117511f432e71d9e9b861a996
Anthony Comstock <comstoan@gmail.com>
2018-10-26 15:06:48 -0700

    more fleshed out struct with methods

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -3,11 +3,25 @@ struct Rectangle{
     height: u32,
 }
 
-fn main() {
-    let rect = Rectangle{width: 30, height: 50};
-    println!("the area is {}",area(&rect));
+impl Rectangle{
+    fn area(&self) -> u32{
+        self.width * self.height
+    }
+    fn can_hold(&self,rect: &Rectangle) -> bool{
+         self.width >= rect.width && self.height >= rect.height 
+    }
+    fn square(size: u32)->Rectangle{
+        Rectangle{width: size, height: size}
+    }
 }
 
-fn area(rect: &Rectangle) -> u32{
-    rect.width * rect.height
+fn main() {
+    let rect = Rectangle{width: 30, height: 50};
+    let rect1 = Rectangle{width: 10, height: 12};
+    let rect2 = Rectangle{width: 10, height: 12};
+    let sq = Rectangle::square(10);
+    println!("can rect1 fit in rect: {}", rect.can_hold(&rect1));
+    println!("can rect1 fit in rect2: {}", rect2.can_hold(&rect1));
+    println!("can rect fit in rect1: {}", rect1.can_hold(&rect));
 }
+
