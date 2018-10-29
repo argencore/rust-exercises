2d1272bed5b4eac0bc68ac594b73398dba8d469d
Anthony Comstock <comstoan@gmail.com>
2018-10-29 12:14:57 -0700

    communicator lib demo

---
+++ communicator/target/debug/deps/communicator-0423c149882b4c02.d
@@ -0,0 +1,5 @@
+/home/jinx/src/rustPrograms/communicator/target/debug/deps/libcommunicator-0423c149882b4c02.rlib: src/lib.rs
+
+/home/jinx/src/rustPrograms/communicator/target/debug/deps/communicator-0423c149882b4c02.d: src/lib.rs
+
+src/lib.rs:
---
+++ communicator/target/debug/libcommunicator.d
@@ -0,0 +1,1 @@
+/home/jinx/src/rustPrograms/communicator/target/debug/libcommunicator.rlib: /home/jinx/src/rustPrograms/communicator/src/lib.rs
