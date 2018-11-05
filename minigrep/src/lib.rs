9bca5ab46d4ff489d10d6dd546e1ef7f9eda40dc
Anthony Comstock <comstoan@gmail.com>
2018-11-05 09:39:39 -0800

    minigrep example finished

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,12 +1,20 @@
 use std::fs;
 use std::error::Error;
+use std::env;
 
-pub fn run(search: Search)-> Result<(),Box<dyn Error>>{
-    println!("in file {}", search.file_path);
+pub fn run(config: Search)-> Result<(),Box<dyn Error>>{
     //read file content to string and unwrap option
-    let content = fs::read_to_string(search.file_path)?;
+    let content = fs::read_to_string(config.file_path)?;
 
-    println!("with text:\n{}", content);
+    let results = if config.case_sensitive {
+        search_case_sensitive(&config.query,&content)
+    }else{
+        search_case_insensitive(&config.query,&content)
+    };
+
+    for line in results{
+        println!("{}", line);
+    }
     Ok(())
         
 }
@@ -15,6 +23,7 @@ pub fn run(search: Search)-> Result<(),Box<dyn Error>>{
 pub struct Search{
    pub query: String,
    pub file_path: String,
+   pub case_sensitive: bool,
 }
 
 impl Search{
@@ -23,6 +32,60 @@ impl Search{
         if args.len() < 3 {
             return Err("not enough arguments");
         }
-        Ok(Search{query: args[1].clone(),file_path: args[2].clone()})
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        Ok(Search{query: args[1].clone(),file_path: args[2].clone(), case_sensitive})
+    }
+}
+
+fn search_case_sensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+    let mut results = Vec::new();
+
+    for line in contents.lines(){
+        if line.contains(query){
+            results.push(line);
+        }
+    }
+    results
+}
+
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+    let mut results = Vec::new();
+    let query = query.to_lowercase();
+
+    for line in contents.lines(){
+        if line.to_lowercase().contains(&query){
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn case_sensitive(){
+        let query = "duct";
+        let contents = "\
+Rust:
+Safe,Fast,Productive.
+Pick three.
+Duct tape";
+        assert_eq!(
+            vec!["Safe,Fast,Productive."],
+            search_case_sensitive(query,contents))
+    }
+    #[test]
+    fn case_insensitive(){
+        let query = "rUsT";
+        let contents = "\
+        Rust:
+Safe,Fast,Productive.
+Pick three.
+Trust me";
+        assert_eq!(
+            vec!["Rust:","Trust me"],
+            search_case_insensitive(query,contents))
     }
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -8,11 +8,11 @@ fn main() {
     //get iterator of command line arguments and then collect into a vector
     let args: Vec<String> = env::args().collect();
     let search = Search::new(&args).unwrap_or_else(|err|{
-        println!("problem parsing arguments:{}", err);
+        eprintln!("problem parsing arguments:{}", err);
         process::exit(1)
     });
     if let Err(e) = minigrep::run(search){
-        println!("application encountered an error:{}",e);
+        eprintln!("application encountered an error:{}",e);
         process::exit(1)
     }
 
