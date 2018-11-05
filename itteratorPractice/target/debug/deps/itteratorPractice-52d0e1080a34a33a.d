cc08472546cb1719ecd5684abba18266e63b06ed
Anthony Comstock <comstoan@gmail.com>
2018-11-05 14:30:42 -0800

    iterator work

---
+++ itteratorPractice/target/debug/deps/itteratorPractice-52d0e1080a34a33a.d
@@ -0,0 +1,5 @@
+/home/jinx/src/rustPrograms/itteratorPractice/target/debug/deps/itteratorPractice-52d0e1080a34a33a: src/main.rs
+
+/home/jinx/src/rustPrograms/itteratorPractice/target/debug/deps/itteratorPractice-52d0e1080a34a33a.d: src/main.rs
+
+src/main.rs:
---
+++ itteratorPractice/target/debug/deps/itteratorPractice-8434d7aed5c11879.d
@@ -0,0 +1,5 @@
+/home/jinx/src/rustPrograms/itteratorPractice/target/debug/deps/itteratorPractice-8434d7aed5c11879: src/main.rs
+
+/home/jinx/src/rustPrograms/itteratorPractice/target/debug/deps/itteratorPractice-8434d7aed5c11879.d: src/main.rs
+
+src/main.rs:
